1f6417c82a40b1cf17019fb60c9c94054a58081a
Irene <isahun7@gmail.com>
2025-11-23 03:42:51 +0100

    contact form almost done

--- Sprint1/js/script.js
+++ Sprint1/js/script.js
@@ -20,16 +20,39 @@ for (let i = 0; i < tabs.length; i++) {
 
 }
 
-const button = document.getElementsByClassName("question-header");
+const buttons = document.getElementsByClassName("question"); /* retorna una col·lecció d'elements amb la classe question */
 
-for (let i = 0; i < coll.length; i++) {
-    button[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+for (let i = 0; i < buttons.length; i++) {
+    const header = buttons[i].querySelector(".question-header"); /* el query selector accepta selectors css complets, i busca dins de cada boto la classe question-header, treu el primer element */
+    
+    header.addEventListener("click", function() {        
+        buttons[i].classList.toggle("active");
+        
+        const arrow = this.querySelector(".arrow"); /* this fa referencia al element clicat */
+        if (this.parentElement.classList.contains("active")) {
+            arrow.style.transform = "rotate(180deg)"; // gira la fletxa cap avall
+            arrow.style.color = "orange"; 
+        } else {
+            arrow.style.transform = "rotate(0deg)"; // torna la fletxa a la posició original
+            arrow.style.color = "#5267DF";
+        }
   });
+}
+
+function validateForm() {
+    const form = document.getElementByClass("contact-form");
+    const userEmail = document.forms["contact-form"]["user-email"].value;
+    const error = document.getElementById("email-error");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+    
+    if (!userEmail) {
+        error.innerHTML = '<img src="bookmark-landing-page/starter-code/images/icon-error.svg" alt="error" style="width:16px;height:16px;"> Has d\'escriure un e-mail.';
+    } else if (!userEmail.includes("@")) {
+        error.innerHTML = '<img src="bookmark-landing-page/starter-code/images/icon-error.svg" alt="error" style="width:16px;height:16px;"> Introdueix un e-mail vàlid.';
+    } else {
+        error.innerHTML = "Formulari enviat amb èxit!"
+    }
+})
 }
